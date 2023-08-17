c4d8bd41d6549129ebbb57288939c5a2992270d2
Eugene Fedchuk <fed4chuck@gmail.com>
2023-08-17 09:08:52 +0300

    cool text

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,7 @@ const Line = gfx.Line;
 
 fn println(
     y: i32,
+    shadow: enum { Top, Bottom },
     comptime fmt: []const u8,
     args: anytype,
 ) void {
@@ -17,11 +18,19 @@ fn println(
     var stream = std.io.fixedBufferStream(&buf);
 
     std.fmt.format(stream.writer(), fmt, args) catch unreachable;
+
+    w4.DRAW_COLORS.* = 3;
+    w4.text(stream.getWritten(), 2, switch (shadow) {
+        .Top => y - 1,
+        .Bottom => y + 1,
+    });
+
+    w4.DRAW_COLORS.* = 2;
     w4.text(stream.getWritten(), 3, y);
 }
 
 var vector = Vec2.create(50, 0);
-var origin = Vec2.create(160 / 2, 160 / 2);
+var origin = Vec2.create(w4.SCREEN_SIZE / 2, w4.SCREEN_SIZE / 2);
 var rotation_rad: f32 = 0;
 
 var started: bool = false;
@@ -46,7 +55,7 @@ fn getMovement() Vec2 {
 }
 
 export fn update() void {
-    if (!started) println(2, "Press z/x/arrows", .{});
+    if (!started) println(2, .Top, "Press z/x/arrows", .{});
 
     const gamepad = w4.GAMEPAD1.*;
     if (gamepad != 0) started = true;
@@ -64,16 +73,16 @@ export fn update() void {
     vector = vector.transform(Mat2.rotate(rotation_rad));
     const line = Line.fromVec2(origin, vector)
         .translate(Vec2.create(vector.x / -2, vector.y / -2));
+    w4.DRAW_COLORS.* = 3;
     line.draw();
 
+    w4.DRAW_COLORS.* = 2;
     if (started) {
-        println(120, "rad/s:{d:.2}", .{rotation_rad * 60});
-        println(130, "O({d:.0},{d:.0})", .{ origin.x, origin.y });
-        println(140, "len:{d:.2}", .{vector.length()});
-        println(150, "mvmt:{d:.2},{d:.2}", .{ mvmt.x, mvmt.y });
+        println(120, .Bottom, "rad/s:{d:.2}", .{rotation_rad * 60});
+        println(130, .Bottom, "O({d:.0},{d:.0})", .{ origin.x, origin.y });
+        println(140, .Bottom, "len:{d:.2}", .{vector.length()});
+        println(150, .Bottom, "mvmt:{d:.2},{d:.2}", .{ mvmt.x, mvmt.y });
     }
 }
 
-export fn start() void {
-    w4.DRAW_COLORS.* = 2;
-}
+export fn start() void {}
